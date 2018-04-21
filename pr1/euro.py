b9f2b88e0fe94fd2e8791b8077b596a837c42a08
ArinaGerashenko <36735006+ArinaGerashenko@users.noreply.github.com>
2018-04-21 19:17:49 +0300

    Update euro.py

--- pr1/euro.py
+++ pr1/euro.py
@@ -1,60 +1,19 @@
-def russian_analog(currency):
-    usd = 57
-    euro = 60
-    bgn = 35
-    huf = 22
-    if currency == 400:
-        count = usd
-        return count
-    elif currency == 401:
-        count = euro
-        return count
-    elif currency == 402:
-        count = bgn
-        return count
-    elif currency == 403:
-        count = huf
-        return count
-    else:
-        count = 0
-        return count
-
-
-def currency_calc(money,currency):
-    usd = 57
-    euro = 60
-    bgn = 35
-    huf = 22
-    if currency == 400:
-        money = round(money / usd, 2)
-        print("Валюта: доллары США")
-        print("При обмене вы получили", money, "единиц выбранной валюты.")
-        return money
-    elif currency == 401:
-        money = round(money / euro, 2)
-        print("Валюта: евро")
-        print("При обмене вы получили", money, "единиц выбранной валюты.")
-        return money
-    elif currency == 402:
-        money = round(money / bgn, 2)
-        print("При обмене вы получили", money, "единиц выбранной валюты.")
-        return money
-    elif currency == 403:
-        money = round(money / huf, 2)
-        print("При обмене вы получили", money, "единиц выбранной валюты.")
-        return money
-    else:
-        money = 0
-        print("Неизвестная валюта")
-    return money
-
-def main():
-    money = 100
-    currency = 401
-
-    result = currency_calc(money,currency)
-    print("курс:", result)
-
-
-if __name__ == "__main__":
-    main()
+def perevod(result):
+	eur = 70
+	dol = 60
+	uah = 2
+	currency = int(input('dollar - 400, euro - 401, uah - 402, \n'))
+	if currency == 400:
+		cash = round (result/dol,2)
+		print ('Доллар:' , cash )
+	elif currency == 401:
+		cash = round (result/eur,2)
+		print ('Евро: ',cash)
+	elif currency == 402:
+		cash = round(result*uah, 2)
+		print('Гривна: ',cash)
+	else:
+		cash = 0
+		print ('Error')
+		return 0
+	return cash
