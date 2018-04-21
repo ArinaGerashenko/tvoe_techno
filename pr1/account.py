90abf22be26dc105465cd117fe1fc700ea13678f
ArinaGerashenko <36735006+ArinaGerashenko@users.noreply.github.com>
2018-04-21 19:16:14 +0300

    Update account.py

--- pr1/account.py
+++ pr1/account.py
@@ -1,21 +1,14 @@
-def calculate_income(rate, money, month):
+def calculate_income(rate,period,money):
     if money <= 0:
         return 0
-
-    for i in range(1, month + 1):
-        money = round(money + money * rate / 100 / 12, 2)
-    return money
-
-
-def main():
+    for i in range (1, period+1):
+        money  = round (money + period*money*(rate/100/12),2)
+        return money
+def main ():
     rate = 10
-    money = 100000
+    money = 10000
     period = 12
-
-    result = calculate_income(rate, money, period)
-    print("Параметры счета:\n", "Сумма: ", money, "\n", "Ставка: ", rate, "\n", "Период: ", period, "\n",
-          "Сумма на счете в конце периода: ", result)
-
-
-if __name__ == "__main__":
+    result = calculate_income(rate, period, money)
+    print ("Параметры счета:\n", "Сумма",money, "\n", "Ставки", rate, "\n", "Период", period, "\n", "Общая сумма", result, "\n")
+if __name__=="__main__":
     main()
