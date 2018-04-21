dcf5d6dd70c47e66a28fc11afaadcd91d3515baf
ArinaGerashenko <36735006+ArinaGerashenko@users.noreply.github.com>
2018-04-21 19:17:04 +0300

    Update calculator.py

--- pr1/calculator.py
+++ pr1/calculator.py
@@ -1,27 +1,25 @@
-print("2 в качестве знака операции завершит работу программы")
-print("Вводите 1 или 0")
+print("0 в качестве знака операции завершит работу программы")
 while True:
-        s = input("Выберите знак операции (and,or,+): ")
-        if s == '2': break
-        if s in ('and','or','+'):
-                x = input("x=")
-                y = input("y=")
-                if (((x!=0) and (x!=1)) or ((y!=0) and (y!=1))):
-                    continue
-                if s == 'and':
-                    if (x==1) and (y==1):
-                        print("1")
-                    else:
-                        print("0")
-                elif s == 'or':
-                    if (x==0) and (y==0):
-                        print("0")
-                    else:
-                        print("1")
-                elif s == '+':
-                    if (x == y):
-                        print("0")
-                    else:
-                        print("1")
-        else:
-            print("Неверный знак")
+    s = input("Выберите операцию (xor,or,and,not): ")
+    if s == '0': break
+    if s in ('xor', 'or', 'and', 'not'):
+        if s == 'xor':
+           x = int(input("x="))
+           y = int(input("y="))
+           print("%.2f" % (x ^ y))
+        elif s == 'or':
+            x = int(input("x="))
+            y = int(input("y="))
+            print("%.2f" % (x | y))
+        elif s == 'and':
+            x = int(input("x="))
+            y = int(input("y="))
+            print("%.2f" % (x & y))
+        elif s == 'not':
+            x = int(input("x="))
+            if x != 0:
+                print("%.2f" % (~x))
+            else:
+                print("Деление на ноль!")
+    else:
+        print("Неверный знак операции!")
